d60624e41aa0bc9e8b11228c8ab593478db30fa6
Stephen Nerby <stephen.a.nerby@gmail.com>
2022-08-03 19:12:36 -0400

    Word Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
     return (
         <nav className="navbar">
-            <h1>PLANTplanIT</h1>
+            <h1>PlanIT PlantIT</h1>
             <NavLink exact to="/">Home</NavLink>
             <NavLink to="/plants">Plants</NavLink>
             <NavLink to="/form">Form</NavLink>
