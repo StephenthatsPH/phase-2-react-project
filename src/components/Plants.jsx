854e3e8b153e16ed72970081c8c837ad23e6189f
Stephen <stephen.a.nerby@gmail.com>
2022-08-03 07:04:41 -0400

    Started Plants component

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,9 +1,20 @@
-
+import { useState } from 'react';
 
 function Plants() {
+    const [plants, setPlants] = useState([
+        {name: 'Rose', price: 5, type: 'flower', id: 1 },
+        {name: 'Ginkgo', price: 15, type: 'tree', id: 2 },
+        {name: 'Boxwood', price: 10, type: 'bush', id: 3 },
+    ])
     return (
         <div>
-            
+            {plants.map((plant) => (
+                <div className="plant-preview" key={plant.id}>
+                    <h2>{ plant.name }</h2>
+                    <p>Type: { plant.type }</p>
+                    <p>${ plant.price }</p>
+                </div>
+            ))}
         </div>
     );
 }
